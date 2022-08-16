2071fe1cf5d91f1284546adea1cfc3c77385ad0e
Mert Doğu <mertdoguonder@protonmail.com>
2022-08-16 14:12:26 +0300

    add: font | privacy policy

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,7 @@ export default function Navbar({text}) {
                 <p className="hidden lg:block dark:text-stone-700">SettleCo. <span className="opacity-40">{text}</span></p>
                 <img src="/logo_transparant.png" className="h-6 w-6 lg:hidden block" alt=""/>
             </div>
-            <div className="text-base font-[400] text-stone-400 dark:text-stone-500 flex space-x-5">
+            <div className="text-base font-[450] text-stone-400 dark:text-stone-500 flex space-x-5">
                 <div className="lg:hidden block">
                     {useNavbar === true ? (
                         <svg onClick={() => setNavbar(false)} className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import { motion } from 'framer-motion';
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
+        <Head>
+            <link href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,1,300,2,601,600,401,501,400,700&display=swap" rel="stylesheet"></link>
+        </Head>
         <Navbar text=""/>
         <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
             pageInitial: {
@@ -18,7 +22,6 @@ function MyApp({ Component, pageProps, router }) {
             <Component {...pageProps} />
         </motion.div>
         <Footer/>
-
     </>
   )
 }
